a52457079748bf9a11944d9e033cd685813f98dc
milleniumbug <milleniumbug@protonmail.com>
2021-12-02 20:00:44 +0100

    Update MagicTranslatorProject importer

--- MemImporter/MagicTranslatorProjectImporter.cs
+++ MemImporter/MagicTranslatorProjectImporter.cs
@@ -17,8 +17,6 @@ namespace MemImporter
         {
             var projectPath = args[0];
 
-            var imagePaths = new Dictionary<string, Guid>();
-            
             if (MagicTranslatorProject.MagicTranslatorProject.Registration.TryOpen(projectPath, out var project, out var failureReason))
             {
                 foreach (var volume in project.Root.Children)
@@ -27,35 +25,27 @@ namespace MemImporter
                     {
                         foreach (var page in chapter.Children.OfType<PageContext>())
                         {
+                            await using var file = File.OpenRead(page.PathToRaw);
+                            await api.AddContextAsync(
+                                Guid.NewGuid().ToString(),
+                                projectName,
+                                "image/png",
+                                page.ShortDescription,
+                                file,
+                                null);
+                            
                             var translations = new List<AddTranslationParams>();
                             foreach (var capture in page.Children)
                             {
-                                bool isNewImage = imagePaths.TryAdd(page.PathToRaw, Guid.NewGuid());
-                                if (isNewImage)
-                                {
-                                    await using var memoryStream = new MemoryStream();
-                                    using var img = await Image.LoadAsync(page.PathToRaw);
-                                    img.Mutate(i =>
-                                    {
-                                        i.Resize(480, 0);
-                                    });
-                                        
-                                    await img.SaveAsPngAsync(memoryStream);
-                                    await api.AddContextsAsync(new AddContextsParams()
-                                    {
-                                        Contexts = new List<AddContextParams>()
-                                        {
-                                            new AddContextParams(imagePaths[page.PathToRaw], memoryStream.ToArray(),
-                                                "image/png", page.ShortDescription)
-                                        }
-                                    });
-                                }
+
                                 translations.Add(new AddTranslationParams()
                                 {
-                                    Context = imagePaths[page.PathToRaw],
                                     CorrelationId = capture.ShortDescription + " " + Guid.NewGuid().ToString(),
                                     Source = capture.Translation.OriginalText,
-                                    Target = capture.Translation.TranslatedText
+                                    Target = capture.Translation.TranslatedText,
+                                    TranslationNotes = new AddTranslationNotesParams(
+                                        capture.Translation.Notes.Select(n => new IoNormalNote("", n.Text)).ToList(),
+                                        capture.Translation.Glosses.Select(n => new IoGlossNote(n.Foreign, n.Text)).ToList())
                                 });
                             }
 

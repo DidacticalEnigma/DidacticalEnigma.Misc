4627c21c2d210a8623c9b866dc75547b4f2cb8e8
milleniumbug <milleniumbug@protonmail.com>
2022-01-15 13:43:38 +0100

    Add data source for Sugoi Japanese Translator

---
+++ DidacticalEnigma.SugoiJapaneseTranslatorDataSource/LruCache.cs
@@ -0,0 +1,152 @@
+using TinyIndex;
+
+namespace DidacticalEnigma.SugoiJapaneseTranslatorDataSource;
+
+public class LruCache<TKey, TValue> : ICache<TKey, TValue>
+        where TKey : notnull
+        where TValue : notnull
+    {
+        private SemaphoreSlim locker = new SemaphoreSlim(1, 1);
+        
+        private readonly Dictionary<TKey, LinkedListNode<LruCacheItem>> cacheMap =
+            new Dictionary<TKey, LinkedListNode<LruCacheItem>>();
+
+        private readonly LinkedList<LruCacheItem> lruList =
+            new LinkedList<LruCacheItem>();
+
+        private readonly Action<TValue>? dispose;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LruCache{TKey, TValue}"/>
+        /// class.
+        /// </summary>
+        /// <param name="capacity">
+        /// Maximum number of elements to cache.
+        /// </param>
+        /// <param name="dispose">
+        /// When elements cycle out of the cache, disposes them. May be null.
+        /// </param>
+        public LruCache(int capacity, Action<TValue>? dispose = null)
+        {
+            this.Capacity = capacity;
+            this.dispose = dispose;
+        }
+
+        /// <summary>
+        /// Gets the capacity of the cache.
+        /// </summary>
+        public int Capacity { get; }
+
+        /// <summary>
+        /// Looks for a value for the matching <paramref name="key"/>. If not found, 
+        /// calls <paramref name="valueGenerator"/> to retrieve the value and add it to
+        /// the cache.
+        /// </summary>
+        /// <param name="key">
+        /// The key of the value to look up.
+        /// </param>
+        /// <param name="valueGenerator">
+        /// Generates a value if one isn't found.
+        /// </param>
+        /// <returns>
+        /// The requested value.
+        /// </returns>
+        public TValue Get(TKey key, Func<TValue> valueGenerator)
+        {
+            try
+            {
+                this.locker.Wait();
+                TValue value;
+                if (this.cacheMap.TryGetValue(key, out var node))
+                {
+                    value = node.Value.Value;
+                    this.lruList.Remove(node);
+                    this.lruList.AddLast(node);
+                }
+                else
+                {
+                    value = valueGenerator();
+                    if (this.cacheMap.Count >= this.Capacity)
+                    {
+                        this.RemoveFirst();
+                    }
+
+                    LruCacheItem cacheItem = new LruCacheItem(key, value);
+                    node = new LinkedListNode<LruCacheItem>(cacheItem);
+                    this.lruList.AddLast(node);
+                    this.cacheMap.Add(key, node);
+                }
+
+                return value;
+            }
+            finally
+            {
+                this.locker.Release();
+            }
+        }
+        
+        public async Task<TValue> GetAsync(TKey key, Func<Task<TValue>> valueGenerator, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                await this.locker.WaitAsync(cancellationToken);
+                TValue value;
+                if (this.cacheMap.TryGetValue(key, out var node))
+                {
+                    value = node.Value.Value;
+                    this.lruList.Remove(node);
+                    this.lruList.AddLast(node);
+                }
+                else
+                {
+                    value = await valueGenerator();
+                    if (this.cacheMap.Count >= this.Capacity)
+                    {
+                        this.RemoveFirst();
+                    }
+
+                    LruCacheItem cacheItem = new LruCacheItem(key, value);
+                    node = new LinkedListNode<LruCacheItem>(cacheItem);
+                    this.lruList.AddLast(node);
+                    this.cacheMap.Add(key, node);
+                }
+
+                return value;
+            }
+            finally
+            {
+                this.locker.Release();
+            }
+        }
+
+        private void RemoveFirst()
+        {
+            // Remove from LRUPriority
+            LinkedListNode<LruCacheItem>? node = this.lruList.First;
+            if (node == null)
+            {
+                return;
+            }
+
+            this.lruList.RemoveFirst();
+
+            // Remove from cache
+            this.cacheMap.Remove(node.Value.Key);
+
+            // dispose
+            this.dispose?.Invoke(node.Value.Value);
+        }
+
+        private class LruCacheItem
+        {
+            public LruCacheItem(TKey k, TValue v)
+            {
+                this.Key = k;
+                this.Value = v;
+            }
+
+            public TKey Key { get; }
+
+            public TValue Value { get; }
+        }
+    }
---
+++ DidacticalEnigma.SugoiJapaneseTranslatorDataSource/SugoiJapaneseTranslatorDataSource.cs
@@ -0,0 +1,66 @@
+﻿using DidacticalEnigma.Core.Models.DataSources;
+using DidacticalEnigma.Core.Models.Formatting;
+using DidacticalEnigma.SugoiSuite;
+using Optional;
+using TinyIndex;
+
+namespace DidacticalEnigma.SugoiJapaneseTranslatorDataSource;
+
+public class SugoiJapaneseTranslatorDataSource : IDataSource
+{
+    private readonly SugoiOfflineTranslatorClient client;
+
+    public static DataSourceDescriptor Descriptor { get; } = new DataSourceDescriptor(
+        new Guid("7AF17241-41A6-4B06-8A83-CEB16783FA0A"),
+        "Sugoi Japanese Translator",
+        "Sugoi Japanese Translator was created by MingShiba",
+        new Uri("https://discord.gg/XFbWSjMHJh"));
+
+    private LruCache<string, RichFormatting> cache = new LruCache<string, RichFormatting>(16);
+
+    public SugoiJapaneseTranslatorDataSource(
+        SugoiOfflineTranslatorClient client)
+    {
+        this.client = client;
+    }
+    
+    public void Dispose()
+    {
+        
+    }
+
+    public async Task<Option<RichFormatting>> Answer(Request request, CancellationToken token)
+    {
+        var text = request.AllText();
+        return (await cache.GetAsync(text, Translate(text, token), token)).Some();
+    }
+
+    private Func<Task<RichFormatting>> Translate(string text, CancellationToken token)
+    {
+        return async () =>
+        {
+            var translated = await this.client.GetTranslation(text);
+
+            return new RichFormatting()
+            {
+                Paragraphs =
+                {
+                    new TextParagraph()
+                    {
+                        Content =
+                        {
+                            new Text(translated)
+                        }
+                    }
+                }
+            };
+        };
+    }
+
+    public Task<UpdateResult> UpdateLocalDataSource(CancellationToken cancellation = new CancellationToken())
+    {
+        return Task.FromResult(UpdateResult.NotSupported);
+    }
+
+    public string? InstanceIdentifier => null;
+}
